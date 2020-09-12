7abc85716d20f579ea5a3f6f367ed3e3c5c21e89
Hitomi Chu <hitomichutatematsu@keio.jp>
2020-09-13 02:28:40 +0900

    Initial Commit

---
+++ Techtagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Techtagram
+//
+//  Created by Hitomi Chu on 13/9/2020.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
